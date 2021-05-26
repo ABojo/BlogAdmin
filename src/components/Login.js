1a85bb53a6e05126194ab7c8eacd7aff640f9bc2
ABojo <alex.bojorquez2598@gmail.com>
2021-05-26 01:02:24 -0400

    pulled in message component and added an error message to the login screen if there user has left either field empty

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,32 +1,54 @@
 import { useState } from 'react';
+import Message from './Message';
 
 function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const resetScreen = () => {
+    setUsername('');
+    setPassword('');
+    setErrorMessage('');
+  };
+
+  const submitLogin = () => {
+    if (username && password) {
+      resetScreen();
+    } else {
+      setErrorMessage('Sorry, you must fill out both fields first!');
+    }
+  };
 
   return (
     <div className="w-full h-screen bg-blue-400">
-      <div className="inline-block p-10 max-w-sm w-11/12 login-form shadow-lg rounded bg-white ">
-        <h1 className="text-center text-blue-500 text-5xl mb-5">
-          <i className="fas fa-lock"></i>
-        </h1>
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.currentTarget.value)}
-          className="p-3 bg-gray-100 block rounded w-full mb-5 text-gray-900 placeholder-gray-600"
-        />
-        <input
-          type="text"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.currentTarget.value)}
-          className="p-3 bg-gray-100 block rounded w-full mb-5 text-gray-900 placeholder-gray-600"
-        />
-        <button className="bg-blue-500 hover:bg-blue-600 transition duration-200 font-bold p-3 rounded w-full text-white">
-          Login
-        </button>
+      <div className="login-form max-w-sm w-11/12 ">
+        {errorMessage && <Message text={errorMessage} success={false} />}
+        <div className="inline-block p-10 w-full shadow-lg rounded bg-white ">
+          <h1 className="text-center text-blue-500 text-5xl mb-5">
+            <i className="fas fa-lock"></i>
+          </h1>
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.currentTarget.value)}
+            className="p-3 bg-gray-100 block rounded w-full mb-5 text-gray-900 placeholder-gray-600"
+          />
+          <input
+            type="text"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.currentTarget.value)}
+            className="p-3 bg-gray-100 block rounded w-full mb-5 text-gray-900 placeholder-gray-600"
+          />
+          <button
+            onClick={submitLogin}
+            className="bg-blue-500 hover:bg-blue-600 transition duration-200 font-bold p-3 rounded w-full text-white"
+          >
+            Login
+          </button>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/Message.js
@@ -0,0 +1,13 @@
+function Message(props) {
+  const { text, success } = props;
+  const backgroundColor = success ? 'bg-green-100' : 'bg-red-100';
+  const textColor = success ? 'text-green-900' : 'text-red-900';
+
+  return (
+    <div className={`p-5 w-full ${backgroundColor} rounded mb-10`}>
+      <h1 className={`${textColor} text-center`}>{text}</h1>
+    </div>
+  );
+}
+
+export default Message;
