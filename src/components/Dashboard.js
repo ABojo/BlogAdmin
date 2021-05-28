4fe2a115b77a50bdcc38e6039a0773ee0967a097
ABojo <alex.bojorquez2598@gmail.com>
2021-05-28 13:19:42 -0400

    added posts state variable to dashboard component + added the use effect hook to fetch the logged in users posts from the API

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,4 +1,16 @@
+import API from '../utils/API';
+import { useState, useEffect } from 'react';
+
 function Dashboard() {
+  const [posts, setPosts] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      const json = await API.getMyPosts();
+      setPosts(json.data.posts);
+    })();
+  }, []);
+
   return <h1>Dashboard</h1>;
 }
 
