9fd22a111524df53365c183a2f0170dd0dc00519
ABojo <alex.bojorquez2598@gmail.com>
2021-05-28 13:34:01 -0400

    Created a basic markup for the dashboard page

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -11,7 +11,33 @@ function Dashboard() {
     })();
   }, []);
 
-  return <h1>Dashboard</h1>;
+  return (
+    <div className="px-5">
+      <h1 className="text-3xl font-bold mb-5 block">
+        Your posts ({posts && posts.length})
+      </h1>
+      {posts &&
+        posts.map((post, i) => {
+          return (
+            <div className="mb-5">
+              <h1 className="text-xl inline">
+                {i + 1}. {post.title}
+              </h1>
+              <a
+                href="/"
+                className="text-yellow-900 bg-yellow-100 px-4 py-2 rounded font-bold ml-3"
+              >
+                Edit
+              </a>
+
+              <a href="/" className="text-gray-500 ml-3">
+                Delete
+              </a>
+            </div>
+          );
+        })}
+    </div>
+  );
 }
 
 export default Dashboard;
