f719de5e9b092907d318b36682efbd4e01b6db11
ABojo <alex.bojorquez2598@gmail.com>
2021-05-28 14:41:20 -0400

    Further updated styling

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -12,30 +12,41 @@ function Dashboard() {
   }, []);
 
   return (
-    <div className="px-5">
-      <h1 className="text-3xl font-bold mb-5 block">
-        Your posts ({posts && posts.length})
+    <div className="px-5 max-w-lg w-11/12 mx-auto">
+      <h1 className="text-3xl mb-5 block">
+        Your Posts ({posts && posts.length})
       </h1>
       {posts &&
-        posts.map((post, i) => {
-          return (
-            <div className="mb-5">
-              <h1 className="text-xl inline">
-                {i + 1}. {post.title}
-              </h1>
-              <a
-                href="/"
-                className="text-yellow-900 bg-yellow-100 px-4 py-2 rounded font-bold ml-3"
-              >
-                Edit
-              </a>
+        posts
+          .map((post, i) => {
+            return (
+              <div className="mb-5 bg-gray-50 p-5 rounded">
+                <h1 className="text-xl inline font-bold">{post.title}</h1>
+                <h1>
+                  <i className="fas fa-comment text-blue-100"></i>{' '}
+                  {post.comments.length}
+                </h1>
+                <h1>
+                  <i
+                    class="fa fa-calendar text-blue-100"
+                    aria-hidden="true"
+                  ></i>
+                  {post.timestamp}
+                </h1>
+                <a
+                  href="/"
+                  className="text-yellow-900 bg-yellow-100 px-4 py-2 rounded font-bold inline-block"
+                >
+                  Edit
+                </a>
 
-              <a href="/" className="text-gray-500 ml-3">
-                Delete
-              </a>
-            </div>
-          );
-        })}
+                <a href="/" className="text-gray-500 ml-3">
+                  Delete
+                </a>
+              </div>
+            );
+          })
+          .reverse()}
     </div>
   );
 }
