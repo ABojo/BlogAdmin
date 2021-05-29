8b7bf225172412a15001cdcbcf9ec9d9ede3ca93
ABojo <alex.bojorquez2598@gmail.com>
2021-05-29 16:38:21 -0400

    Made styling update to title

--- src/components/DeletePopUp.js
+++ src/components/DeletePopUp.js
@@ -10,7 +10,7 @@ function DeletePopUp(props) {
   return (
     <div className="fixed h-screen w-screen bg-opacity-40 bg-black top-0 left-0 z-10">
       <div className="rounded p-5 max-w-3xl w-11/12 bg-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        <h1 className="text-2xl mb-5 border-gray-200 border-b pb-5">
+        <h1 className="text-xl mb-5 border-gray-200 border-b pb-5">
           Are you sure you want to delete {title}?
         </h1>
         <div className="ml-auto block w-max	">
