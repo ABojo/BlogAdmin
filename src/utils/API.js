428c150239b6f3f898bf81dafc79ab5c8565b130
ABojo <alex.bojorquez2598@gmail.com>
2021-05-28 20:44:34 -0400

    added a create post function to the API module

--- src/utils/API.js
+++ src/utils/API.js
@@ -26,6 +26,21 @@ const getMyPosts = async () => {
   return json;
 };
 
-const API = { login, getMyPosts };
+const createPost = async (title, body) => {
+  const raw = await fetch('https://odingblogapi.herokuapp.com/api/posts', {
+    method: 'POST',
+    body: JSON.stringify({ title, body, published: true }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+      Authorization: `Bearer ${localStorage.getItem('authToken')}`,
+    },
+  });
+
+  const json = await raw.json();
+
+  return json;
+};
+
+const API = { login, getMyPosts, createPost };
 
 export default API;
