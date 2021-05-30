8cd5b77644f8c752d714ed6490757f45c0d835b9
ABojo <alex.bojorquez2598@gmail.com>
2021-05-29 20:17:35 -0400

    Added post component to postlist component

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,5 +1,6 @@
 import API from '../utils/API';
 import { useState, useEffect } from 'react';
+import Post from './Post';
 import Loader from './Loader';
 import DeletePopUp from './DeletePopUp';
 
@@ -51,43 +52,7 @@ function PostList(props) {
       )}
       {posts &&
         posts
-          .map((post) => {
-            return (
-              <div
-                className="mb-10 p-8 bg-blue-50 rounded relative"
-                key={post._id}
-              >
-                <h1 className="text-2xl font-bold mb-3">{post.title}</h1>
-                <p className="text-gray-400">
-                  {`${
-                    post.body.length > 200 ? post.body.slice(0, 200) : post.body
-                  }...`}
-                </p>
-                <div className="bg-white inline-block p-3 rounded-full shadow-md text-md absolute bubble">
-                  <h1 className="text-blue-500">
-                    <i className="fas fa-comment mr-1"></i>
-                    {post.comments.length}
-                  </h1>
-                </div>
-
-                <div className="mt-3">
-                  <a
-                    href="/"
-                    className="text-yellow-900 bg-yellow-100 hover:bg-yellow-200 transition duration-200 px-4 py-2 rounded font-bold inline-block"
-                  >
-                    Edit
-                  </a>
-
-                  <button
-                    onClick={() => showDeletePopUp(post._id, post.title)}
-                    className="hover:cursor-pointer ml-3 px-4 py-2 border text-gray-500 border-gray-200 rounded"
-                  >
-                    Delete
-                  </button>
-                </div>
-              </div>
-            );
-          })
+          .map((post) => <Post post={post} showDeletePopUp={showDeletePopUp} />)
           .reverse()}
     </div>
   );
