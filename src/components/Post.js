71504585e5a7bbed166aa54fc5f5bf3331c5556c
ABojo <alex.bojorquez2598@gmail.com>
2021-05-30 20:23:38 -0400

    Added the functionality to let users edit their posts, give them feedback based on its success, disabled buttons based on whether or not the request is pending, and update the post body to the posted data if the request came back with a success status

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,68 @@
+import React, { useState } from 'react';
+import API from '../utils/API';
+import Loader from './Loader';
+import Message from './Message';
+
 function Post(props) {
   const { post, showDeletePopUp } = props;
+  const [editMode, setEditMode] = useState(false);
 
-  return (
-    <div className="mb-10 p-8 bg-blue-50 rounded relative" key={post._id}>
-      <h1 className="text-2xl font-bold mb-3">{post.title}</h1>
+  //Used to set the post info while not in edit mode if the user successfully edits the post
+  const [postTitle, setPostTitle] = useState(post.title);
+  const [postBody, setPostBody] = useState(post.body);
+
+  //tracks the inputs while in edit mode
+  const [titleInput, setTitleInput] = useState(post.title);
+  const [bodyInput, setBodyInput] = useState(post.body);
+
+  //Used to show a loader while a request is pending
+  const [isLoading, setIsLoading] = useState(false);
+
+  //Shows the user a feedback message based on the success/failure of their action
+  const [popUp, setPopUp] = useState({ message: null, success: null });
+
+  const clearPopUp = () => {
+    setPopUp({ message: null, success: null });
+  };
+
+  const toggleEditMode = () => {
+    setEditMode(!editMode);
+  };
+
+  const onClickSave = async () => {
+    clearPopUp();
+    setIsLoading(true);
+    const json = await API.editPost(post._id, {
+      title: titleInput,
+      body: bodyInput,
+    });
+    setIsLoading(false);
+
+    if (json.status === 'failure') {
+      setPopUp({
+        message: 'Sorry, there was a problem updating your post!',
+        success: false,
+      });
+    } else {
+      setPostTitle(titleInput);
+      setPostBody(bodyInput);
+
+      setPopUp({
+        message: 'You have succesfully edit your post!',
+        success: true,
+      });
+    }
+  };
+
+  const disabledBtn = 'bg-gray-100 text-gray-400 cursor-default';
+  const activeBtn =
+    'text-green-900 bg-green-100 hover:bg-green-200 transition duration-200';
+
+  const normalMarkup = (
+    <React.Fragment>
+      <h1 className="text-2xl font-bold mb-3">{postTitle}</h1>
       <p className="text-gray-400">
-        {`${post.body.length > 200 ? post.body.slice(0, 200) : post.body}...`}
+        {`${postBody.length > 200 ? postBody.slice(0, 200) : postBody}...`}
       </p>
       <div className="bg-white inline-block p-3 rounded-full shadow-md text-md absolute bubble">
         <h1 className="text-blue-500">
@@ -15,12 +72,12 @@ function Post(props) {
       </div>
 
       <div className="mt-3">
-        <a
-          href="/"
+        <button
+          onClick={toggleEditMode}
           className="text-yellow-900 bg-yellow-100 hover:bg-yellow-200 transition duration-200 px-4 py-2 rounded font-bold inline-block"
         >
           Edit
-        </a>
+        </button>
 
         <button
           onClick={() => showDeletePopUp(post._id, post.title)}
@@ -29,6 +86,71 @@ function Post(props) {
           Delete
         </button>
       </div>
+    </React.Fragment>
+  );
+
+  const editModeMarkup = (
+    <React.Fragment>
+      {popUp.message && (
+        <Message text={popUp.message} success={popUp.success} />
+      )}
+      {isLoading && (
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <Loader />
+        </div>
+      )}
+      <input
+        type="text"
+        placeholder="Title"
+        onChange={(e) => {
+          setTitleInput(e.currentTarget.value);
+        }}
+        value={titleInput}
+        className="text-md font-bold mb-3 p-3 rounded w-full"
+      />
+      <textarea
+        placeholder="Title"
+        onChange={(e) => {
+          setBodyInput(e.currentTarget.value);
+        }}
+        value={bodyInput}
+        className="text-gray-400 p-3 rounded w-full resize-none h-64"
+      />
+      <div className="bg-white inline-block p-3 rounded-full shadow-md text-md absolute bubble">
+        <h1 className="text-blue-500">
+          <i className="fas fa-comment mr-1"></i>
+          {post.comments.length}
+        </h1>
+      </div>
+
+      <div className="mt-3">
+        <button
+          disabled={isLoading}
+          onClick={() => {
+            clearPopUp();
+            toggleEditMode();
+          }}
+          className="hover:cursor-pointer mr-3 px-4 py-2 border text-gray-500 border-gray-200 rounded"
+        >
+          Cancel
+        </button>
+
+        <button
+          disabled={isLoading}
+          onClick={onClickSave}
+          className={`${
+            isLoading ? disabledBtn : activeBtn
+          } px-4 py-2 rounded font-bold inline-block`}
+        >
+          Save
+        </button>
+      </div>
+    </React.Fragment>
+  );
+
+  return (
+    <div className="mb-10 p-8 bg-blue-50 rounded relative">
+      {editMode ? editModeMarkup : normalMarkup}
     </div>
   );
 }
