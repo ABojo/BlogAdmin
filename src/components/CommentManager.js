8266d6ba7aaa372fd3e17885c04132c441f7b9b0
ABojo <alex.bojorquez2598@gmail.com>
2021-06-03 16:22:12 -0400

    Added a comment manager component and added necessary state variables to make it pop up in the post component

---
+++ src/components/CommentManager.js
@@ -0,0 +1,11 @@
+function CommentManager() {
+  return (
+    <div className="fixed h-screen w-screen bg-opacity-40 bg-black top-0 left-0 z-10">
+      <div className="rounded p-5 max-w-3xl w-11/12 bg-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        test
+      </div>
+    </div>
+  );
+}
+
+export default CommentManager;
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,25 +1,34 @@
 import React, { useState } from 'react';
 import PostEditForm from './PostEditForm';
 import PostDetails from './PostDetails';
+import CommentManager from './CommentManager';
 
 function Post(props) {
   const { post, showDeletePopUp } = props;
   const [editMode, setEditMode] = useState(false);
-
   const [postTitle, setPostTitle] = useState(post.title);
   const [postBody, setPostBody] = useState(post.body);
+  const [commentManager, setCommentManager] = useState(false);
 
   const toggleMode = () => {
     setEditMode(!editMode);
   };
 
+  const toggleCommentMananger = () => {
+    setCommentManager(!commentManager);
+  };
+
   return (
     <div className="mb-10 p-8 bg-blue-50 rounded relative shadow-md">
+      {commentManager && <CommentManager />}
       <div className="bg-white inline-block p-3 rounded-full shadow-md text-md absolute bubble">
         <h1 className="text-blue-500">
           <i className="fas fa-comment mr-1"></i>
           {post.comments.length}
-          <i class="ml-2 fas fa-external-link-alt text-blue-200 text-sm hover:text-blue-400 transition duration-200 cursor-pointer"></i>
+          <i
+            class="ml-2 fas fa-external-link-alt text-blue-200 text-sm hover:text-blue-400 transition duration-200 cursor-pointer"
+            onClick={toggleCommentMananger}
+          ></i>
         </h1>
       </div>
       {editMode ? (
