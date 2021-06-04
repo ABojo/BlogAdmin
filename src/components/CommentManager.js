50365dac593fb90d2767bbf7cfaeaf34cb3d02c4
ABojo <alex.bojorquez2598@gmail.com>
2021-06-03 21:56:04 -0400

    moved the popup message to the top of the comment manager box

--- src/components/CommentManager.js
+++ src/components/CommentManager.js
@@ -9,13 +9,15 @@ function CommentManager(props) {
   return (
     <div className="fixed h-screen w-screen bg-opacity-40 bg-black top-0 left-0 z-10">
       <div className="rounded p-6 max-w-xl w-11/12 bg-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        {popUp.message && (
+          <Message text={popUp.message} success={popUp.success} />
+        )}
+
         <button className="mb-3" onClick={hideManager}>
           <i className="fas fa-arrow-left text-blue-500 text-2xl"></i>
         </button>
         <h1 className="font-bold text-md mb-3">Comments ({comments.length})</h1>
-        {popUp.message && (
-          <Message text={popUp.message} success={popUp.success} />
-        )}
+
         <div className="overflow-y-scroll max-h-96">
           {comments
             .map((comment) => (
