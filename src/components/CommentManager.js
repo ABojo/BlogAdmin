401839fdb3b94171e1e02fdeba351d14066fd5eb
ABojo <alex.bojorquez2598@gmail.com>
2021-06-03 20:35:56 -0400

    Continued adding markup for comment manager

--- src/components/CommentManager.js
+++ src/components/CommentManager.js
@@ -1,8 +1,31 @@
-function CommentManager() {
+import formatDate from '../utils/formatDate';
+
+function CommentManager(props) {
+  const { comments, hideManager } = props;
+  console.log(comments);
   return (
     <div className="fixed h-screen w-screen bg-opacity-40 bg-black top-0 left-0 z-10">
-      <div className="rounded p-5 max-w-3xl w-11/12 bg-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        test
+      <div className="rounded p-6 max-w-xl w-11/12 bg-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <button className="mb-3" onClick={hideManager}>
+          <i className="fas fa-arrow-left text-blue-500 text-2xl"></i>
+        </button>
+        <h1 className="font-bold text-md mb-3">Comments ({comments.length})</h1>
+        <div className="overflow-y-scroll max-h-96">
+          {comments
+            .map((com) => (
+              <div className="mb-3 border-t border-gray-300 pt-3">
+                <h1 className="font-bold text-sm mb-1">{com.name}</h1>
+                <p className="text-sm text-gray-400 mb-1">
+                  {formatDate(com.timestamp)}
+                </p>
+                <p className="text-sm text-gray-500 mb-1">{com.body}</p>
+                <button className="text-red-500 hover:underline transition duration-200 font-bold text-sm">
+                  Delete
+                </button>
+              </div>
+            ))
+            .reverse()}
+        </div>
       </div>
     </div>
   );
--- src/components/Post.js
+++ src/components/Post.js
@@ -8,6 +8,7 @@ function Post(props) {
   const [editMode, setEditMode] = useState(false);
   const [postTitle, setPostTitle] = useState(post.title);
   const [postBody, setPostBody] = useState(post.body);
+  const [postComments, setPostComments] = useState(post.comments);
   const [commentManager, setCommentManager] = useState(false);
 
   const toggleMode = () => {
@@ -20,11 +21,16 @@ function Post(props) {
 
   return (
     <div className="mb-10 p-8 bg-blue-50 rounded relative shadow-md">
-      {commentManager && <CommentManager />}
+      {commentManager && (
+        <CommentManager
+          comments={postComments}
+          hideManager={toggleCommentMananger}
+        />
+      )}
       <div className="bg-white inline-block p-3 rounded-full shadow-md text-md absolute bubble">
         <h1 className="text-blue-500">
           <i className="fas fa-comment mr-1"></i>
-          {post.comments.length}
+          {postComments.length}
           <i
             class="ml-2 fas fa-external-link-alt text-blue-200 text-sm hover:text-blue-400 transition duration-200 cursor-pointer"
             onClick={toggleCommentMananger}
