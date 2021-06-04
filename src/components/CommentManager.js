62f9d5bd739ee39be0020756028efbe8856e9066
ABojo <alex.bojorquez2598@gmail.com>
2021-06-03 21:49:11 -0400

    Added pop up message state variables and condiionally render it based on the whether or not a message has been set

--- src/components/CommentManager.js
+++ src/components/CommentManager.js
@@ -1,7 +1,11 @@
+import { useState } from 'react';
 import Comment from './Comment';
+import Message from './Message';
 
 function CommentManager(props) {
   const { comments, hideManager, removeComment } = props;
+  const [popUp, setPopUp] = useState({ message: null, success: null });
+
   return (
     <div className="fixed h-screen w-screen bg-opacity-40 bg-black top-0 left-0 z-10">
       <div className="rounded p-6 max-w-xl w-11/12 bg-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
@@ -9,6 +13,9 @@ function CommentManager(props) {
           <i className="fas fa-arrow-left text-blue-500 text-2xl"></i>
         </button>
         <h1 className="font-bold text-md mb-3">Comments ({comments.length})</h1>
+        {popUp.message && (
+          <Message text={popUp.message} success={popUp.success} />
+        )}
         <div className="overflow-y-scroll max-h-96">
           {comments
             .map((comment) => (
@@ -16,6 +23,7 @@ function CommentManager(props) {
                 removeComment={removeComment}
                 comment={comment}
                 key={comment._id}
+                setPopUp={setPopUp}
               />
             ))
             .reverse()}
